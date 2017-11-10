c352b0a3c0b1a49adc39bc59cd6fd155f8aa900a
Diego <diego.vid.eco@gmail.com>
2017-11-10 00:09:30 -0600

    compiles to babel

---
+++ src/trd.js
@@ -0,0 +1,3 @@
+const pipe = (fn, ...fns) => (...args) => fns.reduce((acc, fn) => fn(acc), fn(...args));
+const thread  =  (thing, ...fns) => pipe(...fns).apply(null, (thing))
+module.exports = {thread, pipe}
--- trd.js
+++
@@ -1,7 +0,0 @@
-const pipe = (fn, ...fns) => 
-    (...args) => fns.reduce((acc, fn) => fn(acc), fn(...args));
-
-const thread  =  (thing, ...fns) => 
-    pipe(...fns).apply(null, (thing))
-
-module.exports = thread
